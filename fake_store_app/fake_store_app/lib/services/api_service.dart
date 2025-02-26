bfc876938c6e2947091d3ac6898a89859a4a7419
Youngmin Cho <choyoungmin0322@gmail.com>
2025-02-26 11:43:00 +0900

    제품 ID 타입 변경

--- fake_store_app/fake_store_app/lib/services/api_service.dart
+++ fake_store_app/fake_store_app/lib/services/api_service.dart
@@ -19,4 +19,13 @@ class ApiService {
       throw 'Failed to load products';
     }
   }
+
+  Future<Product> getProduct(int id) async {
+    final response = await http.get(Uri.parse('$_baseUrl/products/$id'));
+    if (response.statusCode == 200) {
+      return Product.fromJson(jsonDecode(response.body));
+    } else {
+      throw 'Failed to load product';
+    }
+  }
 }
