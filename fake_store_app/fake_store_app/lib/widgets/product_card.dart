3e9321bc4fe8c780ec2fcfa3e3d6eb97f9a8d22d
Youngmin Cho <choyoungmin0322@gmail.com>
2025-02-26 09:51:14 +0900

    커머스 앱 프로젝트 생성 및 API, 제품 모델, 상품 목록, 제품 카드 위젯 추가

---
+++ fake_store_app/fake_store_app/lib/widgets/product_card.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+
+  const ProductCard({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      child: ListTile(
+        leading: Image.network(
+          product.image,
+          width: 50,
+          height: 50,
+          fit: BoxFit.cover,
+        ),
+        title: Text(product.title),
+        subtitle: Text('\$${product.price.toStringAsFixed(2)}'),
+      ),
+    );
+  }
+}
