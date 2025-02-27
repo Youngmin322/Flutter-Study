ceb1ced84d956167ae2ce5ba039cd04dd2987c77
Youngmin Cho <choyoungmin0322@gmail.com>
2025-02-27 15:40:06 +0900

    장바구니 화면 개선 및 삭제 기능 추가

--- fake_store_app/fake_store_app/lib/screens/cart_screen.dart
+++ fake_store_app/fake_store_app/lib/screens/cart_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/cart_item.dart';
+import '../providers/cart_provider.dart';
+import '../widgets/cart_item_widget.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -17,7 +22,37 @@ class CartScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(child: Text('장바구니 화면')),
+      body: SafeArea(
+        child: Consumer<CartProvider>(
+          builder: (context, cart, child) {
+            if (cart.itemCount == 0) {
+              return const Center(child: Text('장바구니에 상품이 없습니다.'));
+            }
+            return Column(
+              children: [
+                // 장바구니 상품 목록
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: cart.items.length,
+                    itemBuilder: (context, index) {
+                      // 장바구니 상품 정보
+                      CartItem cartItem = cart.items[index];
+                      return CartItemWidget(cartItem: cartItem);
+                    },
+                  ),
+                ),
+                // 결제하기 버튼
+                ElevatedButton(
+                  onPressed: () {
+                    // 결제하기
+                  },
+                  child: Text('결제하기 - \$${cart.totalPrice.toStringAsFixed(2)}'),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
